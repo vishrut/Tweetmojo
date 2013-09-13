57f0d7de25c55f127c3ba8b5e45310fb56bb1c4a
vishrut <vrp101@gmail.com>
2013-09-13 14:06:34 +0530

    Validating offer details before sending to Instamojo

--- tweetrieve/models.py
+++ tweetrieve/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Tweet(models.Model):
-	user_handle = models.CharField(max_length=200)
-	tweet_text = models.CharField(max_length=200)
+	user_handle = models.CharField()
+	tweet_text = models.TextField()
 	tweet_id = models.IntegerField()
--- tweetrieve/views.py
+++ tweetrieve/views.py
@@ -8,6 +8,7 @@ import json
 import requests
 from models import Tweet
 import oauth2 as oauth
+import re
 
 CONSUMER_KEY = "RA8zeFfkz1jpTy5I6P2g"
 CONSUMER_SECRET = "IytfyvKxmPBnrRmqxt0NV0waoXUPq8rfUXD0tzg3HI"
@@ -15,7 +16,61 @@ CONSUMER_SECRET = "IytfyvKxmPBnrRmqxt0NV0waoXUPq8rfUXD0tzg3HI"
 OAUTH_TOKEN = "87239233-VSblo33EEkuQyNHgi11GRe0QqSHXPBM3X6tBRAWk"
 OAUTH_TOKEN_SECRET = "xu05xY5WXxPoctvljLLbnoVSGSGfp0Yt241r6EVrd0"
 
-tweet_list = []
+X_APP_ID = "37674edc3f8d743425ddd747fb6ccf5c" 
+X_AUTH_TOKEN = "a974800a5567e2a22759c8176b3ddb80"
+
+translation_dict = {
+			't': 'title',
+			'd': 'description',
+			'c': 'currency',
+			'p': 'base_price',
+			'q': 'quantity',
+			's': 'start_date',
+			'e': 'end_date',
+			'z': 'timezone',
+			'v': 'venue',
+			'u': 'redirect_url',
+			'n': 'note',
+			'f': 'file_upload_json',
+			'i': 'cover_image_json',
+			'title': 'title',
+			'currency': 'currency',
+			'price': 'base_price',
+		}
+
+"""
+validation_dict = {
+			'title': validate_title,
+			#'description': validate_description,
+			'currency': validate_currency,
+			'base_price': validate_price,
+			'quantity': validate_quantity,
+			'start_date': validate_start_date, 
+			'end_date': validate_end_date, 
+			#'timezone': validate_timezone,
+			#'venue': validate_venue,
+			#'redirect_url': validate_url,
+			#'note': validate_note, 
+			#'file_upload_json': validate_file_json,
+			#'cover_image_json': validate_cover,
+		}
+"""
+messages = {
+			'title': 'Title required, should not be empty', #not empty
+			#'description': '',
+			'currency': 'Currency required and must be INR or USD',# INR or USD
+			'base_price': 'Base price must be a number', #must be number
+			'quantity': 'Quantity must be a number', #must be number
+			'start_date': 'Start date format should be YYYY-MM-DD HH:MM', #strptime
+			'end_date': 'End date format should be be YYYY-MM-DD HH:MM', #strptime and > start
+			#'timezone': 'Timezone format - Asia/Kolkata', #not empty
+			'date_trio': 'For an event, start date, end date and timezone should occur together.'
+			#'venue': '',
+			#'redirect_url': '',
+			#'note': '',
+			#'file_upload_json': '',
+			#'cover_image_json': '',
+		}
 
 
 def home(request):
@@ -25,41 +80,38 @@ def home(request):
 	return HttpResponse(template.render(context))
 
 def tweetsubmit(request):
+	tweet_text = request.POST['tweet_text'].encode('ascii','ignore')
+	tweet_text = """t-hi\nc-INR\np-50\ns-abc\n"""
+	regex = re.compile(r'[\r]')
+	tweet_text = regex.sub('', tweet_text)
+	
+	param_list = tweet_text.split("\n")
+	offer_dict = {}
+	for param in param_list:
+		pair = param.split('-', 1)
+		if len(pair)==2:
+			field = pair[0].strip().lower()
+			value = pair[1].strip()
+			if field in translation_dict:
+				offer_key = translation_dict[field]
+				offer_dict[offer_key] = value
+	
+	error_messages = validate_offer(offer_dict)
 
 	headers = {
-           'X-App-Id': '37674edc3f8d743425ddd747fb6ccf5c',
-           'X-Auth-Token': 'a974800a5567e2a22759c8176b3ddb80'
+           'X-App-Id': X_APP_ID,
+           'X-Auth-Token': X_AUTH_TOKEN 
 		}
-
-	payload	= {
-			'title': 'Test Membership through API 2',
-			#'description': 'Testing API',
-			'currency': 'INR',
-			'base_price': '50',
-			'quantity': '0',
-			'start_date': '2013-12-31 23:00',
-			'end_date': '2014-12-31 23:59',
-			'timezone': 'Asia/Kolkata',
-			'venue': 'Goa',
-			#'redirect_url':,
-			#'note':,
-			#'file_upload_json':,
-			#'cover_image_json':,
-		}
-	## Mandatory Fields - Title, Currency, Base Price
-	## Events - Start Date, End Date, Timezone
-	## Files - File Upload JSON
-	1. Check for TCB
-	2. If start or end date then start, end dates and timezone should exist
-
-
-	r = requests.post('https://www.instamojo.com/api/1/offer/', headers=headers, data=payload)
+	
+	#r = requests.post('https://www.instamojo.com/api/1/offer/', headers=headers, data=offer_dict)
 
 	template = loader.get_template('create_offer.html')
 	context = RequestContext(request, {
 		'tweet': request.POST['tweet_text'],
-		'api_response': r.text
+		#'api_response': r.text,
+		'error_messages': error_messages
 	})
+	
 	return HttpResponse(template.render(context))
 
 def formsubmit(request):
@@ -77,18 +129,11 @@ def formsubmit(request):
 	)
 
 	content_dict = json.loads(content)
-
-	headers = {
-           'X-App-Id': '37674edc3f8d743425ddd747fb6ccf5c',
-           'X-Auth-Token': 'a974800a5567e2a22759c8176b3ddb80'
-		}
-	
-	#r = requests.post('https://www.instamojo.com/api/1/debug/', headers=headers)
-	#print r.text
 	
 	status_list = content_dict['statuses']
 	tweet_id = 0
 	for status in status_list:
+		#print status['text'].encode('utf-8')
 		new_tweet = Tweet(tweet_id = tweet_id, user_handle="Vishrut42", tweet_text=status['text'])
 		tweet_list.append(new_tweet)
 		tweet_id+=1
@@ -100,3 +145,26 @@ def formsubmit(request):
 	})
 	
 	return HttpResponse(template.render(context))
+
+def validate_offer(offer_dict):
+	error_messages = []
+	if 'title' not in offer_dict:
+		error_messages.append('Title required.')
+	if 'currency' not in offer_dict:
+		error_messages.append('Currency required.')
+	if 'base_price' not in offer_dict:
+		error_messages.append('Base price required.')
+	
+	if 'start_date' in offer_dict or 'end_date' in offer_dict:
+		if ('start_date' not in offer_dict or
+			'end_date' not in offer_dict or
+			'timezone' not in offer_dict):
+			error_messages.append(messages['date_trio'])
+
+	"""
+	for field in offer_dict:
+		if field in validation_dict:
+			if not validation_dict[field](offer_dict[field]):
+				error_messages.append(messages[field])
+	"""
+	return error_messages
